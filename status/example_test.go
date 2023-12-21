cdbc5c976e45ed73df1e48ffc3eaeeeb2015ab23
Timothy Stiles <tim@stiles.io>
2023-12-21 12:14:09 -0800

    updated status example test.

--- status/example_test.go
+++ status/example_test.go
@@ -3,5 +3,12 @@ package status_test
 import "github.com/TimothyStiles/buster/status"
 
 func ExampleCheck() {
-	status.Check()
+	err := status.Check()
+
+	if err != nil {
+		panic(err)
+	}
+	// Output:
+	// Building with Dagger
+	// dagger runtime is healthy
 }
