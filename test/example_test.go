81ba0c2445ea06de30ffdd72665780020d413036
Timothy Stiles <tim@stiles.io>
2023-12-22 13:45:45 -0800

    removed dagger from workflows.

--- test/example_test.go
+++ test/example_test.go
@@ -2,6 +2,7 @@ package test_test
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/TimothyStiles/buster/test"
 )
@@ -11,7 +12,11 @@ func ExampleTest() {
 	testCommand := []string{"go", "test", "./test/mock/mock_test.go"}
 	results, _ := test.Test(testCommand)
 
-	fmt.Println(results)
-	// Output:
-	// "ok  \tcommand-line-arguments\t0.001s\n"
+	// split the results on whitespace and remove empty strings
+	resultsSlice := strings.Fields(results)
+
+	// print the first two words of the results since the last will be variable based on how fast the tests run
+	result := fmt.Sprintf("%s %s", resultsSlice[0], resultsSlice[1])
+	fmt.Println(result)
+	// Output: ok command-line-arguments
 }
--- test/test.go
+++ test/test.go
@@ -1,58 +1,44 @@
 package test
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
-
-	"dagger.io/dagger"
 )
 
 // Test is a function that runs a test command in a container
-func Test(exec []string) (string, error) {
-	ctx := context.Background()
+func Test(cmdString []string) (string, error) {
 
-	if len(exec) == 0 {
+	if len(cmdString) == 0 {
 		return "", fmt.Errorf("no command provided")
-	} else if len(exec) == 1 {
-		exec = append([]string{"go", "test"}, exec...)
-	} else if exec[0] != "go" && exec[1] != "test" {
-		exec = append([]string{"go", "test"}, exec...)
+	} else if len(cmdString) == 1 {
+		cmdString = append([]string{"go", "test"}, cmdString...)
+	} else if cmdString[0] != "go" && cmdString[1] != "test" {
+		cmdString = append([]string{"go", "test"}, cmdString...)
 	}
-
-	// initialize Dagger client
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
-	if err != nil {
-		return "", err
-	}
-	defer client.Close()
 
 	absRepoPath, err := findGoModOrGit()
+
 	if err != nil {
 		log.Println(err)
 	}
-	// get the source code to test
-	src := client.Host().Directory(absRepoPath)
 
-	// get the default container image for golang
-	golang := client.Container().From("golang:1.21")
-
-	// mount cloned repository into `golang` image
-	golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
-
-	// Print the contents of the mounted directory
-	golang = golang.WithExec(exec)
-
-	// run the container
-	results, err := golang.Stdout(ctx)
-
-	if err != nil {
-		return results, err
+	// change the working directory to the absolute path of the repo
+	if err := os.Chdir(absRepoPath); err != nil {
+		log.Println(err)
 	}
 
-	return results, nil
+	// execute go test
+	cmd := exec.Command(cmdString[0], cmdString[1:]...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to execute command: %v", err)
+	}
+
+	return string(output), nil
+
 }
 
 // this is a helper function to help find the absolute path across different operating systems and environments
